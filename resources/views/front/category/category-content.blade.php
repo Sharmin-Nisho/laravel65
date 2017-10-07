15faba027158158db6a1669bd3e7a92a13045b71
Sharmin Nisho <sharmin.nisho@gmail.com>
2017-10-07 21:11:32 +0600

    first commit

---
+++ resources/views/front/category/category-content.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 10/7/2017
+ * Time: 7:35 PM
+ */
---
+++ resources/views/front/includes/header.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 10/7/2017
+ * Time: 6:49 PM
+ */
